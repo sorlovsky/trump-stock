06a78ca6f552e1898c94370bbc3e556c8b866d64
Sabastian Mugazambi <mugazambi@gmail.com>
2017-05-29 23:28:04 -0500

    Commenting

--- prediction_knn.py
+++ prediction_knn.py
@@ -1,28 +1,41 @@
-
-import numpy as np
-import matplotlib.pyplot as plt
-import pylab
-import sys
-import math
-
+'''
+* @What Data Mining Final Project : Using Tweeter sentiment analysis to predict change in stock price.
+* @Author Sabastian Mugazambi & Simon Orlovsky
+* @Date 05/27/2017
+* @Purpose : Use KNN to predict the change in stock price.
+'''
 
 def euclidean(k,train,newpoint):
-
+    """
+    @Function :  euclidean(k,train,newpoint)
+    @Args: <k> <training data set points> <the new point to predict change>
+    @Purpose: find average changes from knearest neighbors
+    """
 	cal_dis = list()
     tickers_list = list()
+    pec_change = list()
 
 	for key, value in d.iteritems():
         tickers_list.append(key)
         y = value[0]
+        pec_change = value[1]
 		cal_dis.append(math.sqrt(np.sum(np.subtract(newpoint,y)**2)))
 
 	voters = np.argsort(cal_dis,axis=-1,kind="mergesort",order=None)
 	nearest_stocks = list()
+    nearest_pc_changes = list()
 
 	for i in range(0,k):
-		nearest_stocks.append(tickers_list[voters[i]])
+        v = voters[i]
+		nearest_stocks.append(tickers_list[v])
+        nearest_pc_changes.append(pec_change[v])
 
-	return nearest_stocks
+	return np.mean(nearest_stocks)
 
 def run_knn(k,train,newpoint):
+    """
+    @Function :  run_knn(k,train,newpoint)
+    @Args: <k> <training data set points> <the new point to predict change>
+    @Purpose: makes call to the euclidean distance method of finding KNN
+    """
     return euclidean(train,newpoint,k))
--- tweet_dumper.py
+++ tweet_dumper.py
@@ -19,47 +19,52 @@ access_secret = "8hYzrUpnpmVXRpdW40eMqdGusudo6ItfDOE2Alk1Tkd9o"
 
 
 def get_all_tweets(screen_name):
-	#Twitter only allows access to a users most recent 3240 tweets with this method
+    """
+    @Function :  get_all_tweets(screen_name)
+    @Args: <user name>
+    @Purpose: gets all the tweets of given user id
+    """
+    #Twitter only allows access to a users most recent 3240 tweets with this method
 
-	#authorize twitter, initialize tweepy
-	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	auth.set_access_token(access_key, access_secret)
-	api = tweepy.API(auth)
+    #authorize twitter, initialize tweepy
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
 
-	#initialize a list to hold all the tweepy Tweets
-	alltweets = []
+    #initialize a list to hold all the tweepy Tweets
+    alltweets = []
 
-	#make initial request for most recent tweets (200 is the maximum allowed count)
-	new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+    #make initial request for most recent tweets (200 is the maximum allowed count)
+    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
 
-	#save most recent tweets
-	alltweets.extend(new_tweets)
+    #save most recent tweets
+    alltweets.extend(new_tweets)
 
-	#save the id of the oldest tweet less one
-	oldest = alltweets[-1].id - 1
+    #save the id of the oldest tweet less one
+    oldest = alltweets[-1].id - 1
 
-	#keep grabbing tweets until there are no tweets left to grab
-	while len(new_tweets) > 0:
-		print "getting tweets before %s" % (oldest)
+    #keep grabbing tweets until there are no tweets left to grab
+    while len(new_tweets) > 0:
+        print "getting tweets before %s" % (oldest)
 
-		#all subsiquent requests use the max_id param to prevent duplicates
-		new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+        #all subsiquent requests use the max_id param to prevent duplicates
+        new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
 
-		#save most recent tweets
-		alltweets.extend(new_tweets)
+        #save most recent tweets
+        alltweets.extend(new_tweets)
 
-		#update the id of the oldest tweet less one
-		oldest = alltweets[-1].id - 1
+        #update the id of the oldest tweet less one
+        oldest = alltweets[-1].id - 1
 
-		print "...%s tweets downloaded so far" % (len(alltweets))
+        print "...%s tweets downloaded so far" % (len(alltweets))
 
-	#transform the tweepy tweets into a 2D array that will populate the csv
-	outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
+    #transform the tweepy tweets into a 2D array that will populate the csv
+    outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
 
-	#write the csv
-	with open('%s_tweets.csv' % screen_name, 'wb') as f:
-		writer = csv.writer(f)
-		writer.writerow(["id","created_at","text"])
-		writer.writerows(outtweets)
+    #write the csv
+    with open('%s_tweets.csv' % screen_name, 'wb') as f:
+        writer = csv.writer(f)
+        writer.writerow(["id","created_at","text"])
+        writer.writerows(outtweets)
 
-	pass
+    pass
